b177f9a102b15050b05634aa660a15a58ae86ed2
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-12-06 20:38:23 +0200

    refactor redis client

--- utils/redis.js
+++ utils/redis.js
@@ -1,54 +1,47 @@
-import { createClient } from "redis";
+import { createClient } from 'redis';
 import { promisify } from 'util';
 
 class RedisClient {
-
-    #client;
-    #get;
-
-    constructor() {
+  constructor() {
+    this.isClientConnected = true;
+    this.client = createClient()
+      .on('error', (err) => {
+        console.log('Error connecting to Redis', err);
+        this.isClientConnected = false;
+      })
+      .on('ready', () => {
         this.isClientConnected = true;
-        this.#client = createClient()
-            .on('error', (err) => {
-                console.log('Error connecting to Redis', err);
-                this.isClientConnected = false;
-            })
-            .on('ready', () => {
-                // console.log('Redis client connected');
-                this.isClientConnected = true;
-            })
+      });
 
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    this.setAsync = promisify(this.client.set).bind(this.client);
+    this.delAsync = promisify(this.client.del).bind(this.client);
+  }
 
-        this.#get = promisify(this.#client.get).bind(this.#client);
-        this.setAsync = promisify(this.#client.set).bind(this.#client);
-        this.delAsync = promisify(this.#client.del).bind(this.#client);
-    }
+  isAlive() {
+    return this.isClientConnected;
+  }
 
-    isAlive() {
-        return this.isClientConnected;
-    }
+  async get(key) {
+    /* eslint-disable  no-unused-vars */
+    const value = await this.getAsync(key).then((response) => response).catch((err) => { });
+    return value;
+  }
 
-    async get(key) {
-        return await this.#get(key).then((response) => response).catch((err) => { });
-    }
+  async set(key, value, duration) {
+    /* eslint-disable  no-unused-vars */
+    await this.setAsync(key, value)
+      .then((response) => {
+      })
+      .catch((err) => { });
+    this.client.expire(key, duration);
+  }
 
-    async set(key, value, duration) {
-        await this.setAsync(key, value)
-            .then((response) => {
-                // console.log('Created with success');
-            })
-            .catch((err) => { console.log(err); });
-        this.#client.expire(key, duration);
-    }
-
-    async del(key) {
-        await this.delAsync(key)
-            .then(() => { console.log('Deleted with success'); })
-            .catch((err) => { console.log(err); });
-    }
-
-
-
+  async del(key) {
+    await this.delAsync(key)
+      .then(() => { console.log('Deleted with success'); })
+      .catch((err) => { console.log(err); });
+  }
 }
 
 const redisClient = new RedisClient();
