c4e692815434ea5f38c65147f93297be63c6274d
ussumane <ussumane.momade@uem.ac.mz>
2024-02-11 14:46:16 +0200

    fix: linter errors

--- utils/redis.js
+++ utils/redis.js
@@ -1,34 +1,35 @@
-import { createClient } from "redis";
+import { createClient } from 'redis';
+
 const util = require('util');
 
 class RedisClient {
-    constructor () {
-        this.client = createClient().on('error', (e) => {
-            console.log(e.message);
-            this.isConnected = false;
-        }).on('ready', () => {
-            this.isConnected = true;
-        });
+  constructor() {
+    this.client = createClient().on('error', (e) => {
+      console.log(e.message);
+      this.isConnected = false;
+    }).on('ready', () => {
+      this.isConnected = true;
+    });
 
-        this.get = util.promisify(this.client.get).bind(this.client);
-    }
+    this.get = util.promisify(this.client.get).bind(this.client);
+  }
 
-    isAlive() {
-        return this.client.connected;
-    }
+  isAlive() {
+    return this.client.connected;
+  }
 
-    async get(key) {
-        const value = this.client.get(key);
-        return value;
-    }
+  async get(key) {
+    const value = this.client.get(key);
+    return value;
+  }
 
-    async set(key, value, duration) {
-        await this.client.set(key, value, 'EX', duration);
-    }
+  async set(key, value, duration) {
+    await this.client.set(key, value, 'EX', duration);
+  }
 
-    async del(key) {
-        await this.client.del(key);
-    }
+  async del(key) {
+    await this.client.del(key);
+  }
 }
 
 export default new RedisClient();
